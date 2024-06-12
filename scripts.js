3a5c88089ecc67eadfa70b8294be2fe4f21f7da1
JacqueADev <jacquelineadev@gmail.com>
2024-06-12 17:49:55 -0300

    Adicionando a função onclick
    Adicionei a função onclick no javascript.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("Funcionou")
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+
+function cliqueiNoBotao(){
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
