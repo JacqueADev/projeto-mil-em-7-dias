b6f4394ee309bc27411033fab6056d235c4fe84a
JacqueADev <jacquelineadev@gmail.com>
2024-06-17 16:40:32 -0300

    Finalização do Javascript + formulário ativo

--- scripts.js
+++ scripts.js
@@ -3,8 +3,14 @@ const mascara = document.querySelector(".mascara-formulario")
 
 
 
-function cliqueiNoBotao(){
+function mostrarForm(){
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
     mascara.style.visibility = "visible"
+}
+
+function esconderForm(){
+    form.style.left = "-300px"
+    form.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
 }
